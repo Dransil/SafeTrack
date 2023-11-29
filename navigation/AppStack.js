f06099d13e647b2836c448303044fde75d281e32
Emanuel Taquichiri <thecobra1783@gmail.com>
2023-11-29 00:10:53 -0400

    BottomTabsScreen

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -1,14 +1,23 @@
 import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import { MenuInicio } from '../screen';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
+function TabsPrueba (){
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name='Home' component={MenuInicio}/>
+    </Tab.Navigator>
+  );
+}
 export const AppStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name='Menu' component={ MenuInicio } />
+      <Stack.Screen name='Menu' component={ TabsPrueba } />
     </Stack.Navigator>
   );
 };
