1ad2a7c7fc5bc049b3a1df85f395a9a8550b3499
Emanuel Taquichiri <thecobra1783@gmail.com>
2023-12-02 23:44:00 -0400

    textinput nombre contacto

--- screen/ContactoScreen.js
+++ screen/ContactoScreen.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Alert } from 'react-native';
 export const ContactoScreen = () =>{
-    
+    return(
+        <View>
+            <TextInput
+                placeholder="Nombre contacto"
+            />
+        </View>
+    )
 }
